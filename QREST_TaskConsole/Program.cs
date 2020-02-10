25da8a89f0c0b57742f5a5e1d8520f4270fee55f
Doug Timms <doug.timms@open-environment.org>
2020-02-10 00:20:51 -0500

    Phase 2 finish

--- QREST_TaskConsole/Program.cs
+++ QREST_TaskConsole/Program.cs
@@ -2,11 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QREST_TaskConsole
 {
